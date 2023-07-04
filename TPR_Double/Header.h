f242cb06e62516223b499c7ab5d7e9c7ea88f411
Aleksey Smirnov <112711924+abuyabva@users.noreply.github.com>
2023-07-04 18:55:02 +0300

    Add files via upload

---
+++ TPR_Double/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <Windows.h>
+#include <cmath>
+
+using namespace std;
+
+void firstTeorem();
+void secondTeorem();
+void thirdTheorem();
